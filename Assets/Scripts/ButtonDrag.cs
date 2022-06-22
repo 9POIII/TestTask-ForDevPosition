53ccbbf52ac4521be913227752850f64b91ab7a2
9POIII <ryklik95@gmail.com>
2022-06-22 16:10:37 +0300

    finished project

---
+++ Assets/Scripts/ButtonDrag.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class ButtonDrag : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private RectTransform m_RectTransform;
+    private Camera m_MainCamera;
+    private float m_xStartButtonCoord, m_yStartButtonCoord;
+    
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+    [SerializeField] private bool m_Available;
+    [SerializeField] private Grid m_Grid;
+    [SerializeField] private int m_ButtonNumber;
+    
+    public Building buildingPrefab;
+
+    private void Awake()
+    {
+        m_xStartButtonCoord = gameObject.GetComponent<RectTransform>().localPosition.x;
+        m_yStartButtonCoord = gameObject.GetComponent<RectTransform>().localPosition.y;
+        m_RectTransform = GetComponent<RectTransform>();
+        m_Grid.grid = new Building[m_Grid.gridSize.x, m_Grid.gridSize.y];
+        m_MainCamera = Camera.main;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        m_RectTransform.anchoredPosition += eventData.delta;
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        var groundPlane = new Plane(Vector3.up, Vector3.zero);
+        Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (groundPlane.Raycast(ray, out float position))
+        {
+            Vector3 worldPosition = ray.GetPoint(position);
+
+            x = Mathf.RoundToInt(worldPosition.x);
+            y = Mathf.RoundToInt(worldPosition.z);
+
+            m_Available = true;
+
+            if (x < 0 || x > m_Grid.gridSize.x - buildingPrefab.size.x) m_Available = false;
+            if (y < 0 || y > m_Grid.gridSize.y - buildingPrefab.size.y) m_Available = false;
+
+            if (m_Available && m_Grid.IsPlaceTaken(x, y, m_ButtonNumber)) m_Available = false;    
+            
+            if (m_Available)
+            {
+                m_Grid.PlaceFlyingBuilding(x, y, m_ButtonNumber);
+                Instantiate(buildingPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                Destroy(gameObject);
+            }
+            else
+            {
+                gameObject.GetComponent<RectTransform>().localPosition =
+                    new Vector3(m_xStartButtonCoord, m_yStartButtonCoord, 0);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour
+{
+    private bool m_PopUpPanelBool;
+    
+    public Vector2Int gridSize = new Vector2Int(20, 20);
+    public Building[,] grid;
+    
+    [SerializeField] private ButtonDrag[] m_ButtonDrag;
+    [SerializeField] private GameObject m_PopUpPanel;
+    [SerializeField] private Text m_BuildingText;
+    
+    
+    private void Awake()
+    {
+        grid = new Building[gridSize.x, gridSize.y];
+        m_PopUpPanel.SetActive(false);
+        m_PopUpPanelBool = true;
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click();
+        }
+    }
+
+    void Click()
+    {
+        if (m_PopUpPanelBool)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+ 
+            if (Physics.Raycast (ray, out hit))
+            {
+                m_PopUpPanel.SetActive(true);
+                if (hit.collider.gameObject.CompareTag("Plane"))
+                {
+                    m_BuildingText.text = "There is no building";
+                    m_PopUpPanelBool = false;
+                }
+                else
+                {
+                    m_BuildingText.text = "Building name: " + hit.collider.gameObject.name;
+                    m_PopUpPanelBool = false;
+                }
+            }
+        }
+    }
+
+    public void ExitButton()
+    {
+        m_PopUpPanel.SetActive(false);
+        m_PopUpPanelBool = true;
+    }
+
+
+    public bool IsPlaceTaken(int placeX, int placeY, int numberReturn)
+    {
+        for (int x = 0; x < m_ButtonDrag[numberReturn].buildingPrefab.size.x; x++)
+        {
+            for (int y = 0; y < m_ButtonDrag[numberReturn].buildingPrefab.size.y; y++)
+            {
+                if (grid[placeX + x, placeY + y] != null) return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void PlaceFlyingBuilding(int placeX, int placeY, int numberReturn)
+    {
+        for (int x = 0; x < m_ButtonDrag[numberReturn].buildingPrefab.size.x; x++)
+        {
+            for (int y = 0; y < m_ButtonDrag[numberReturn].buildingPrefab.size.y; y++)
+            {
+                grid[placeX + x, placeY + y] = m_ButtonDrag[numberReturn].buildingPrefab;
+            }
+        }
+    }
+}
+
